03d501ffb2c703df6d8514ca63b1320a031d3f47
JASON_NEWTON_ALBERT <126969936+JasonAlbertNewton@users.noreply.github.com>
2023-06-25 13:47:26 +0800

    Proxy Server template

---
+++ mainserver.c
@@ -0,0 +1,11 @@
+//header files
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <resolv.h>
+#include <string.h>
+#include <pthread.h>
+#include <unistd.h>
+
+int main(){
+    return 0;
+}
